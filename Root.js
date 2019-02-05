2e7ac51055c8b56c2a8c36b06c5e877a5ca09ba0
Karan9889 <43220806+Karan9889@users.noreply.github.com>
2019-02-05 15:25:27 -0500

    Add files via upload

---
+++ Root.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+
+import HomePage from './app/screens/Home';
+import MapPage from './app/screens/Map';
+
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomePage
+    },
+    Map: {
+      screen: MapPage
+    }
+  },
+  {
+    initialRouteName: 'Home',
+    
+  }
+);
+
